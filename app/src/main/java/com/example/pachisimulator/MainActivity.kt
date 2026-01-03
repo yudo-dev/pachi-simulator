f590bc68db884287be9bd30bce58fefce66c8cce
yudo-dev-admin <kuzuyu280@gmail.com>
2026-01-04 00:57:31 +0900

    ヘッダー作成

--- app/src/main/java/com/example/pachisimulator/MainActivity.kt
+++ app/src/main/java/com/example/pachisimulator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.pachisimulator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // ヘッダー
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
